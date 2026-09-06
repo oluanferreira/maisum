923b45e2d087b408dd4131b983d5db25b0444a10
oluanferreira <104535972+oluanferreira@users.noreply.github.com>
2026-09-06 17:36:53 -0300

    feat(partner): make signup route public

--- apps/restaurant-web/src/middleware.ts
+++ apps/restaurant-web/src/middleware.ts
@@ -44,5 +44,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|login|register).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|login|register|cadastro).*)'],
 }
